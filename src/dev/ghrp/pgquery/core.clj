baf28b5d79015426102aea40fdac957e3ac58b8a
Evgeny Mukha <profunctorlense@gmail.com>
2024-06-19 01:08:57 +0200

    fix paths in deps, add usage example

--- src/dev/ghrp/pgquery/core.clj
+++ src/dev/ghrp/pgquery/core.clj
@@ -1,6 +1,12 @@
 (ns dev.ghrp.pgquery.core
   (:import (dev.ghrp.pgquery PgQueryWrapper)))
 
-(defn parse-sql [sql]
+(defn parse-sql ^#_"JSON"String [^String sql]
   (let [wrapper (PgQueryWrapper.)]
     (.parseSQL wrapper sql)))
+
+(comment
+  ;; Usage example
+  (parse-sql "SELECT 1")
+
+  )
